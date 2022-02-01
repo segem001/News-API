54dc66b1f67bb66c2c176ff1a03bded43cb2d012
segem001 <kipdom001@gmail.com>
2022-02-01 10:46:06 +0300

    Add sources.py file

---
+++ app/sources.py
@@ -0,0 +1,10 @@
+
+class NewsSources:
+
+        """"""
+        def __init__(self,id,title,name,description,url):
+                self.id=id
+                self.title=title
+                self.name = name
+                self.description= description
+                self.url=url
