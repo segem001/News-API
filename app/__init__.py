019a3ae1aece205451dc262fcf56555ca6594c21
segem001 <kipdom001@gmail.com>
2022-02-01 11:35:38 +0300

    Add python files

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config import config_options
+
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    # initializing application
+    app = Flask(__name__)
+    # setting up configuration
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
---
+++ start.sh
@@ -0,0 +1,3 @@
+export API_KEY='cd0842bba1ac47268b6cfb064fbc5c82'
+export SECRET_KEY='This is secret'
+python3.8 manage.py server
